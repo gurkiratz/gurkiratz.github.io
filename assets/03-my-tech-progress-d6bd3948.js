b6920cad58f462345dfd9cb9b46c42100d0141b8
Gurkirat Singh <singh.gurkirat2707@gmail.com>
2023-09-24 02:04:05 -0400

    Updates

---
+++ assets/03-my-tech-progress-d6bd3948.js
@@ -0,0 +1,12 @@
+import{j as e,a as i,F as c}from"./index-dddf9e88.js";const o="My tech progress",l="This is a pretty raw blog...",h="April 10, 2023";function r(t){const n=Object.assign({p:"p",h2:"h2",strong:"strong",ul:"ul",li:"li",h1:"h1"},t.components);return i(c,{children:[e(n.p,{children:"This is a pretty raw blog for me to keep a track of my progress and what i want to achieve in coming months."}),`
+`,e(n.h2,{children:"Worked with"}),`
+`,e(n.p,{children:e(n.strong,{children:"dsf"})}),`
+`,i(n.ul,{children:[`
+`,e(n.li,{children:"React"}),`
+`,e(n.li,{children:"Tailwind"}),`
+`,e(n.li,{children:"Typescript"}),`
+`,e(n.li,{children:"Node.js"}),`
+`,e(n.li,{children:"Express"}),`
+`]}),`
+`,e(n.h1,{children:"Want to learn"}),`
+`,e(n.h2,{children:"Tech"})]})}function a(t={}){const{wrapper:n}=t.components||{};return n?e(n,Object.assign({},t,{children:e(r,t)})):r(t)}export{h as createdAt,a as default,l as description,o as title};
